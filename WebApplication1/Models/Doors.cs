8de0e36b7894a370dad94d67df27472e1dc911c4
TesvonU <fildar2004@gmail.com>
2025-08-09 03:46:45 +0200

    added first door selection

--- WebApplication1/Models/Doors.cs
+++ WebApplication1/Models/Doors.cs
@@ -5,7 +5,11 @@
         private bool door1 = false;
         private bool door2 = false;
         private bool door3 = false;
-        
+        private int selectedDoor1;
+        private int revealedDoor;
+        private int selectedDoor2;
+
+
         private Random random = new Random();
         public Doors()
         {
@@ -26,6 +30,72 @@
                     door3 = true;
                     break;
             }
+
+        }
+
+        public void SelectDoor1(int selected) //sets which door was picked and which was opened
+        {
+            selectedDoor1 = selected;
+            bool carSelected = false;
+            int coin = random.Next(0, 2);
+            switch (selected)
+            {
+                case 0:
+                    if (door1)
+                    {
+                        carSelected = true;
+                        if (coin == 0)
+                        {
+                            revealedDoor = 1;
+                        }
+                        else
+                        {
+                            revealedDoor = 2;
+                        }
+                        
+                    }
+                        break;
+
+                case 1:
+                    if (door2)
+                    {
+                        carSelected = true;
+                        if (coin == 0)
+                        {
+                            revealedDoor = 0;
+                        }
+                        else
+                        {
+                            revealedDoor = 2;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    if (door3)
+                    {
+                        carSelected = true;
+                        revealedDoor = coin;
+                    }
+                    break;
+            }
+            if (!carSelected)
+            {
+                if (!door1 && selected != 0)
+                {
+                    revealedDoor = 0;
+                }
+                if (!door2 && selected != 1)
+                {
+                    revealedDoor = 1;
+                }
+                if (!door3 && selected != 2)
+                {
+                    revealedDoor = 2;
+                }
+            }
+            return;
+
 
         }
 
